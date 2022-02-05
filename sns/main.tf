0accdf1ecc71b30af98c981c410ba399e31c04f9
magaum <weslei.paula@fatec.sp.gov.br>
2022-02-04 23:01:23 -0300

    feat: Adicionada KMS

--- sns/main.tf
+++ sns/main.tf
@@ -1,11 +1,69 @@
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
   profile = "sns_account"
 }
 
+data "aws_caller_identity" "current" {}
+
+resource "aws_kms_key" "this" {
+  description             = "SNS kms"
+  deletion_window_in_days = 10
+  policy                  = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "Allow access for Key Administrator",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "${data.aws_caller_identity.current.account_id}"
+      },
+      "Action": [
+        "kms:GenerateDataKey*",
+        "kms:Decrypt",
+        "kms:Create*",
+        "kms:Describe*",
+        "kms:Enable*",
+        "kms:List*",
+        "kms:Put*",
+        "kms:Update*",
+        "kms:Revoke*",
+        "kms:Disable*",
+        "kms:Get*",
+        "kms:Delete*",
+        "kms:TagResource",
+        "kms:UntagResource",
+        "kms:ScheduleKeyDeletion",
+        "kms:CancelKeyDeletion"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Sid": "Allow access for Key User (SNS Service Principal)",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "sns.amazonaws.com"
+      },
+      "Action": [
+        "kms:GenerateDataKey*",
+        "kms:Decrypt"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+  POLICY
+}
+
+resource "aws_kms_alias" "this" {
+  name          = "alias/sns-kms"
+  target_key_id = aws_kms_key.this.key_id
+}
+
 resource "aws_sns_topic" "this" {
-  name         = "cross-account-sns"
-  display_name = "cross-account-sns"
+  name              = "cross-account-sns"
+  display_name      = "cross-account-sns"
+  kms_master_key_id = aws_kms_alias.this.id
 }
 
 resource "aws_sns_topic_policy" "this" {
--- sqs/main.tf
+++ sqs/main.tf
@@ -3,8 +3,71 @@ provider "aws" {
   profile = "sqs_account"
 }
 
+data "aws_caller_identity" "current" {}
+
+resource "aws_kms_key" "this" {
+  description             = "SQS kms"
+  deletion_window_in_days = 10
+  policy                  = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "Allow access for Key Administrator",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "${data.aws_caller_identity.current.account_id}"
+      },
+      "Action": [
+        "kms:GenerateDataKey*",
+        "kms:Decrypt",
+        "kms:Create*",
+        "kms:Describe*",
+        "kms:Enable*",
+        "kms:List*",
+        "kms:Put*",
+        "kms:Update*",
+        "kms:Revoke*",
+        "kms:Disable*",
+        "kms:Get*",
+        "kms:Delete*",
+        "kms:TagResource",
+        "kms:UntagResource",
+        "kms:ScheduleKeyDeletion",
+        "kms:CancelKeyDeletion"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Sid": "Allow access for Key User (SQS/SNS Service Principals)",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": [
+          "sqs.amazonaws.com",
+          "sns.amazonaws.com"
+        ]
+      },
+      "Action": [
+        "kms:GenerateDataKey*",
+        "kms:Decrypt"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_kms_alias" "this" {
+  name          = "alias/sqs-kms"
+  target_key_id = aws_kms_key.this.key_id
+}
+
+
 resource "aws_sqs_queue" "dlq" {
-  name = "cross-account-dlq"
+  name                              = "cross-account-dlq"
+  kms_master_key_id                 = aws_kms_alias.this.id
+  kms_data_key_reuse_period_seconds = 300
 }
 
 resource "aws_sqs_queue_policy" "dlq_policy" {
@@ -31,11 +94,15 @@ POLICY
 }
 
 resource "aws_sqs_queue" "principal" {
-  name = "cross-account-sqs"
+  name                              = "cross-account-sqs"
+  kms_master_key_id                 = aws_kms_alias.this.id
+  kms_data_key_reuse_period_seconds = 300
+
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.dlq.arn
     maxReceiveCount     = 4
   })
+
   redrive_allow_policy = jsonencode({
     redrivePermission = "byQueue",
     sourceQueueArns   = ["${aws_sqs_queue.dlq.arn}"]
